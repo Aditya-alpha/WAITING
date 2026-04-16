cea400f847d0edb15b926276253d0274b9f2e7a8
Aditya-alpha <bandraladitya5544@gmail.com>
2026-04-16 20:12:45 +0530

    second commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,18 +3,18 @@ import bear from "./assets/bear.png"
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100 px-16 py-6 text-gray-900">
+    <div className="flex flex-col min-h-screen bg-gray-100 sm:px-16 p-4 sm:py-6 text-gray-900">
       <nav>
         <div>
-          <img src={logo} alt="Logo" className="h-12 w-28" />
+          <img src={logo} alt="Logo" className="sm:h-12 sm:w-28 h-8 w-18" />
         </div>
       </nav>
       <div className="flex flex-col items-center gap-20 m-auto" >
         <div className="flex items-center gap-6" >
-          <h1 className="text-5xl mb-4">Fuck paperhands</h1>
-          <img src={bear} alt="Bear" className="w-44 h-40" />
+          <h1 className="sm:text-5xl text-4xl mb-4">Fuck paperhands</h1>
+          <img src={bear} alt="Bear" className="sm:w-44 sm:h-40 w-28 h-26" />
         </div>
-        <p className="text-5xl">CA : XXXXXXXXXXXXXXXXXXXXXX</p>
+        <p className="sm:text-5xl text-3xl">CA : XXXXXXXXXXXXXXXXXXXXXX</p>
       </div>
     </div>
   )
